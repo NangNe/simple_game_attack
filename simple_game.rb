a485d9d0297b836af49050f517253c9cc14afcd0
nangne <nvnang.20it1@vku.udn.vn>
2023-04-01 14:50:13 +0700

    git commit -change

--- simple_game.rb
+++ simple_game.rb
@@ -1,13 +1,17 @@
 require 'byebug'
 class Player
-    attr_accessor :health, :attack_power
-    def initialize(health, attack_power)
+    attr_accessor :health, :attack_power, :skill
+    def initialize(health, attack_power, skill)
         @health = health
         @attack_power = attack_power
+        @skill = skill
     end
 
-    def attack(enemy)
-        damage = rand(50..@attack_power)
+    def attack(enemy, skill_index)
+        # byebug
+        skills = @skill[skill_index]
+        
+        damage = rand(50..@attack_power) + skills
         puts "Player attacks with #{damage} damage"
         enemy.take_damage(damage)
       
@@ -25,13 +29,15 @@ end
 
 
 class Enemy
-    attr_accessor :health, :attack_power
-    def initialize(health, attack_power)
+    attr_accessor :health, :attack_power, :skill
+    def initialize(health, attack_power, skill)
         @health = health
         @attack_power = attack_power
+        @skill = skill
     end
     def attack(player)
-        damage = rand(50..@attack_power)
+        # byebug
+        damage = rand(0..@attack_power) + rand(0..skill)
         puts "Enemy attacks with #{damage} damage"
         player.take_damage(damage)
     end
@@ -47,21 +53,42 @@ class Enemy
     end
 end
 
-player = Player.new(rand(50..100), rand(50..100))
-enemy = Enemy.new(rand(50..100), rand(50..100))
+class Game
+    $player = $player || Player.new(rand(50..200), rand(50..100), [5,10,15])
+    $enemy = $enemy || Enemy.new(rand(50..200), rand(5..50), 50)
+        
+        # enemy = Enemy.new(rand(50..100), rand(50..100), rand(0..10))        
+    
+    def start
+     
+        puts $player.health
 
-puts "Player has #{player.health} health and has #{player.attack_power} attack"
-puts "Enemy has #{enemy.health} health and has #{enemy.attack_power} attack"
+        puts $enemy.health
+        while $player.check_die && $enemy.check_die
+            puts "Player has #{$player.health} health , has #{$player.attack_power} attack and has #{$player.skill} skill"
+            puts "Enemy has #{$enemy.health} health, has #{$enemy.attack_power} attack and has #{$enemy.skill} skill"
+            puts "Select a skill to use:"
+            puts "1. "
+            puts "2. "
+            puts "3. "
+            skill_index = gets.chomp.to_i - 1
+            $player.attack($enemy,skill_index)
+            $enemy.attack($player)
+        end
 
-# Vòng lặp người chơi tấn công trước rồi tới enemy cho tới khi health <0
-while player.check_die && enemy.check_die
-  player.attack(enemy)
-  enemy.attack(player)
+        if $enemy.check_die
+            puts "Enemy wins!"
+          else
+            puts "Player wins!"
+        end
+    end
 end
+
+game = Game.new
+game.start
+# Vòng lặp người chơi tấn công trước rồi tới enemy cho tới khi health <0
+
 
 # Kiểm tra health ?
-if enemy.check_die
-  puts "Enemy wins!"
-else
-  puts "Player wins!"
-end
+
+    
